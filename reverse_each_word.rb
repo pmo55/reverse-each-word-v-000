964006fd96f52152eabf34a0a60387e45eaa0be1
Parsa Moshiri <parsamoshiri5@gmail.com>
2018-07-01 18:41:19 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,12 @@
+def reverse_each_word(string)
+  string_2=[]
+  counter=0 
+  new_string=string.split(" ")
+  new_string.collect do |word|
+  
+   string_2[counter]=word.reverse
+   counter+=1
+  end
+ 
+return string_2.join(" ")  
+end
